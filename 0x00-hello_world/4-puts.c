cbc9ab33842a47946b3c3e97e7a0d5721eca4ccd
Diamond Kanu <diamond kanu>
2023-08-10 14:26:33 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints a sentence followed by a new line
+ * Return : always end with 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle,");
+	return (0);
+}
