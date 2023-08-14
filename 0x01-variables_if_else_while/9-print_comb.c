c27f766347e581d3350c32d42e26056128d368bd
Diamond Kanu <diamond kanu>
2023-08-13 17:17:55 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,11 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i < 9 ; i++)
+	for (i = 0 ; i <= 9 ; i++)
 	{
-		if (i == 9)
-			putchar(i + '0');
-		else
-			putchar(i + '0');
-			putchar(',');
-			putchar(' ');
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
 	}
 	return (0);
 }
