dbe3f9110e5ea9f5255143a24d369743e5617e4f
Diamond Kanu <diamond kanu>
2023-09-21 08:01:13 -0700

    corrected main.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,9 +20,6 @@ typedef struct list_s
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
-int _strlen(const char *s);
-char *_strcpy(char *dest, const char *src);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void before_main(void) __attribute__((constructor));
 #endif
