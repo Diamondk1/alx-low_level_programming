c66ec90f6f2acf2e4515498676c584c7a67cd7da
Diamond Kanu <diamond kanu>
2023-09-21 07:16:53 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - a function that prints the list
+ * @h: the list
+ * Return: (int) The size of the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	char *str;
+
+	while (h != NULL)
+	{
+		printf("[%d] ", h->len);
+		str = h->str;
+		if (str == NULL)
+			str = "(nil)";
+		printf("%s\n", str);
+		h = h->next;
+		++count;
+	}
+	return (count);
+}
