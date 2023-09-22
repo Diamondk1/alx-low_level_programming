972a08c7d55a645f3cb0ea9ca835af68887fff82
Diamond Kanu <diamond kanu>
2023-09-22 09:15:08 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * b_main - function that executes before main
+ *
+ * Return: void
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
